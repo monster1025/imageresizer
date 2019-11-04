376d1bc32a4fae1e1748df4fa64b121b93822e9d
Your Name <you@example.com>
2019-11-05 00:12:42 +0300

    dynamic quality

--- imgresize.py
+++ imgresize.py
@@ -8,7 +8,25 @@ import logging
 from PIL import Image
 from PIL.ExifTags import TAGS
 
-maxImgFileSizeKb = int(os.getenv('MAX_IMG_FILE_SIZE', 1500))
+class file_counter(object):
+    def __init__(self):
+        self.position = self.size = 0
+
+    def seek(self, offset, whence=0):
+        if whence == 1:
+            offset += self.position
+        elif whence == 2:
+            offset += self.size
+        self.position = min(offset, self.size)
+
+    def tell(self):
+        return self.position
+
+    def write(self, string):
+        self.position += len(string)
+        self.size = max(self.size, self.position)
+
+maxImgFileSizeKb = int(os.getenv('MAX_IMG_FILE_SIZE', 2000))
 quality = int(os.getenv('QUALITY', 90))
 divideSize = int(os.getenv('DIVIDE_SIZE', 90))
 folderToProcess = os.getenv('FOLDER', './img/')
@@ -22,7 +40,7 @@ def main():
         log.info("Sleeping {}".format(sleepTime))
         time.sleep(sleepTime)
 
-def findandresize(dir='.', maxImgFileSizeKb=1500):
+def findandresize(dir='.', maxImgFileSizeKb):
     files = []
     counter = 1
     for r, d, f in os.walk(dir):
@@ -32,13 +50,13 @@ def findandresize(dir='.', maxImgFileSizeKb=1500):
                 filePath=os.path.join(r, file)
                 fileSizeKb=int(os.path.getsize(filePath)/1024)
                 if (fileSizeKb>maxImgFileSizeKb):
-                    try:
-                        process_image(filePath, fileSizeKb, counter)
-                    except Exception:
-                        log.error("Error processing {}".format(filePath))
+                    # try:
+                    process_image(filePath, fileSizeKb, counter, maxImgFileSizeKb)
+                    # except Exception:
+                    #     log.error("Error processing {}".format(filePath))
                 counter=counter+1
 
-def process_image(filePath, fileSizeKb, counter):
+def process_image(filePath, fileSizeKb, counter, maxImgFileSizeKb):
     img = Image.open(filePath)
     # exif = img._getexif()
     # model=get_exif_field(exif, 'Model')
@@ -47,7 +65,7 @@ def process_image(filePath, fileSizeKb, counter):
     # if (date == None):
     #     date=creation_date
     # print_exif(exif)
-    resize_image(filePath, img, divideSize, quality)
+    resize_image(filePath, img, divideSize, quality, maxImgFileSizeKb)
     filePath = rename_file(filePath, counter)
     newFileSizeKb=int(os.path.getsize(filePath)/1024)
     log.info("[{} kB - {} kB] {}".format(fileSizeKb, newFileSizeKb, filePath))
@@ -59,13 +77,15 @@ def rename_file(filePath, counter):
     return newFilePath
 
 
-def resize_image(filePath, img, divideSize, quality):
+def resize_image(filePath, img, divideSize, quality, maxImgFileSizeKb):
     exif_bin = None
     if 'exif' in img.info:
       exif_bin = img.info['exif']
     width, height = img.size
     img = img.resize((int(width / 100 * divideSize),int(height / 100 * divideSize)),Image.ANTIALIAS)
-    img.save(filePath, exif=exif_bin, quality=quality)
+    current_quality = guess_quality_for_size(img, maxImgFileSizeKb*1024)
+    log.info("We guess quality for file {}: {}".format(filePath, current_quality))
+    img.save(filePath, exif=exif_bin, quality=current_quality)
 
 def get_exif_field(exif,field):
   if exif == None:
@@ -77,6 +97,17 @@ def get_exif_field(exif,field):
 def print_exif(exif):
   for (k,v) in exif.items():
      log.info("{}: {}".format(TAGS.get(k), v))
+
+def guess_quality_for_size(im, size, guess=70, subsampling=1, low=1, high=100):
+    while low < high:
+        counter = file_counter()
+        im.save(counter, subsampling=subsampling, quality=guess)
+        if counter.size < size:
+            low = guess
+        else:
+            high = guess - 1
+        guess = (low + high + 1) // 2
+    return low
 
 def get_creation_date(path_to_file):
     """
@@ -111,3 +142,4 @@ def get_module_logger(mod_name):
 if __name__ == "__main__":
     log = get_module_logger(__name__)
     main()
+
